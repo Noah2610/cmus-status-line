6f797a33ecf99a081440a957a1a7726629f4dfe0
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-11-26 23:35:57 +0100

    Move FormatPart new module

--- src/cmus_status/output/format/format_part.rs
+++ src/cmus_status/output/format/format_part.rs
@@ -1,82 +1,4 @@
 use super::*;
-use crate::error::prelude::*;
-use regex::Regex;
-use std::convert::TryFrom;
-
-const DEFAULT_FORMAT: &str = r#"
-%{MatchStatus(Playing, "")}
-%{MatchStatus(Paused, "")}
-%{MatchStatus(Stopped, "")} 
-%{MaxLen(60, Title)}  
-%{ProgressBar("<####---->")}
-"#;
-
-#[derive(Deserialize)]
-#[serde(default, try_from = "String")]
-pub struct Format {
-    parts: Vec<FormatPart>,
-}
-
-impl Format {
-    pub fn iter(&self) -> std::slice::Iter<FormatPart> {
-        self.parts.iter()
-    }
-
-    fn try_from_string<S>(string: S) -> MyResult<Self>
-    where
-        S: ToString,
-    {
-        let string = string.to_string();
-        let re = Regex::new(r"(%\{(?P<keyword>.+?)\})|(?P<text>.+?)").unwrap();
-        let mut parts = Vec::new();
-
-        for caps in re.captures_iter(string.as_str()) {
-            if let Some(keyword) = caps.name("keyword") {
-                let keyword = keyword.as_str();
-                let part = ron::de::from_str::<FormatPart>(keyword)
-                    .or(Err(Error::InvalidFormatKeyword(keyword.into())))?;
-                parts.push(part);
-            }
-            if let Some(text) = caps.name("text") {
-                if let Some(prev_text) = parts.last_mut().and_then(|last| {
-                    if let FormatPart::Text(prev_text) = last {
-                        Some(prev_text)
-                    } else {
-                        None
-                    }
-                }) {
-                    prev_text.push_str(text.as_str());
-                } else {
-                    parts.push(FormatPart::Text(text.as_str().into()));
-                }
-            }
-        }
-
-        Ok(Self { parts })
-    }
-}
-
-impl TryFrom<&str> for Format {
-    type Error = Error;
-
-    fn try_from(string: &str) -> MyResult<Self> {
-        Self::try_from_string(string)
-    }
-}
-
-impl TryFrom<String> for Format {
-    type Error = Error;
-
-    fn try_from(string: String) -> MyResult<Self> {
-        Self::try_from_string(string)
-    }
-}
-
-impl Default for Format {
-    fn default() -> Self {
-        Format::try_from(DEFAULT_FORMAT).unwrap()
-    }
-}
 
 #[derive(Deserialize)]
 pub enum FormatPart {
@@ -86,20 +8,6 @@ pub enum FormatPart {
     MatchStatus(CmusPlaybackStatus, String),
     MaxLen(usize, Box<FormatPart>), // Inclusive
     ProgressBar(ProgressBarConfig),
-}
-
-impl FormatPart {
-    fn is_text(&self) -> bool {
-        if let FormatPart::Text(_) = self {
-            true
-        } else {
-            false
-        }
-    }
-
-    fn is_keyword(&self) -> bool {
-        !self.is_text()
-    }
 }
 
 #[derive(Deserialize, Debug)]
---
+++ src/cmus_status/output/format/mod.rs
@@ -0,0 +1,83 @@
+mod format_part;
+
+pub use format_part::FormatPart;
+
+use super::*;
+use crate::error::prelude::*;
+use regex::Regex;
+use std::convert::TryFrom;
+
+const DEFAULT_FORMAT: &str = r#"
+%{MatchStatus(Playing, "")}
+%{MatchStatus(Paused, "")}
+%{MatchStatus(Stopped, "")} 
+%{MaxLen(60, Title)}  
+%{ProgressBar("<####---->")}
+"#;
+
+#[derive(Deserialize)]
+#[serde(default, try_from = "String")]
+pub struct Format {
+    parts: Vec<FormatPart>,
+}
+
+impl Format {
+    pub fn iter(&self) -> std::slice::Iter<FormatPart> {
+        self.parts.iter()
+    }
+
+    fn try_from_string<S>(string: S) -> MyResult<Self>
+    where
+        S: ToString,
+    {
+        let string = string.to_string();
+        let re = Regex::new(r"(%\{(?P<keyword>.+?)\})|(?P<text>.+?)").unwrap();
+        let mut parts = Vec::new();
+
+        for caps in re.captures_iter(string.as_str()) {
+            if let Some(keyword) = caps.name("keyword") {
+                let keyword = keyword.as_str();
+                let part = ron::de::from_str::<FormatPart>(keyword)
+                    .or(Err(Error::InvalidFormatKeyword(keyword.into())))?;
+                parts.push(part);
+            }
+            if let Some(text) = caps.name("text") {
+                if let Some(prev_text) = parts.last_mut().and_then(|last| {
+                    if let FormatPart::Text(prev_text) = last {
+                        Some(prev_text)
+                    } else {
+                        None
+                    }
+                }) {
+                    prev_text.push_str(text.as_str());
+                } else {
+                    parts.push(FormatPart::Text(text.as_str().into()));
+                }
+            }
+        }
+
+        Ok(Self { parts })
+    }
+}
+
+impl TryFrom<&str> for Format {
+    type Error = Error;
+
+    fn try_from(string: &str) -> MyResult<Self> {
+        Self::try_from_string(string)
+    }
+}
+
+impl TryFrom<String> for Format {
+    type Error = Error;
+
+    fn try_from(string: String) -> MyResult<Self> {
+        Self::try_from_string(string)
+    }
+}
+
+impl Default for Format {
+    fn default() -> Self {
+        Format::try_from(DEFAULT_FORMAT).unwrap()
+    }
+}

6659374600178419c0ab6c953ae0ff7e88c236db
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-11-26 23:51:18 +0100

    Split CmusData fields into modules

--- src/cmus_status/data/mod.rs
+++ src/cmus_status/data/mod.rs
@@ -1,16 +1,19 @@
+mod playback_status;
+mod time;
+
 pub mod prelude {
     pub use super::CmusData;
     pub use super::CmusPlaybackStatus;
-    pub use super::CmusTime;
-    pub use super::Seconds;
+    pub use super::{CmusTime, Seconds};
 }
+
+pub use playback_status::CmusPlaybackStatus;
+pub use time::{CmusTime, Seconds};
 
 use crate::error::prelude::*;
 use std::convert::TryFrom;
 use std::fmt;
 use std::path::PathBuf;
-
-pub type Seconds = u32;
 
 #[derive(Debug)]
 pub struct CmusData {
@@ -123,56 +126,6 @@ impl TryFrom<String> for CmusData {
             // TODO
             settings: CmusSettings {},
         })
-    }
-}
-
-#[derive(Debug, PartialEq, Deserialize)]
-pub enum CmusPlaybackStatus {
-    Playing,
-    Paused,
-    Stopped,
-}
-
-impl TryFrom<&str> for CmusPlaybackStatus {
-    type Error = Error;
-
-    fn try_from(status_str: &str) -> Result<Self, Self::Error> {
-        const STATUS_PLAYING: &str = "playing";
-        const STATUS_PAUSED: &str = "paused";
-        const STATUS_STOPPED: &str = "stopped";
-
-        match status_str.to_lowercase().as_str() {
-            STATUS_PLAYING => Ok(CmusPlaybackStatus::Playing),
-            STATUS_PAUSED => Ok(CmusPlaybackStatus::Paused),
-            STATUS_STOPPED => Ok(CmusPlaybackStatus::Stopped),
-            s => Err(Error::CmusUnknownStatus(s.into())),
-        }
-    }
-}
-
-impl fmt::Display for CmusPlaybackStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                CmusPlaybackStatus::Playing => "Playing",
-                CmusPlaybackStatus::Paused => "Paused",
-                CmusPlaybackStatus::Stopped => "Stopped",
-            }
-        )
-    }
-}
-
-#[derive(Debug)]
-pub struct CmusTime {
-    pub duration: Seconds,
-    pub position: Seconds,
-}
-
-impl CmusTime {
-    pub fn completion_percentage(&self) -> f32 {
-        self.position as f32 / self.duration as f32
     }
 }
 
---
+++ src/cmus_status/data/playback_status.rs
@@ -0,0 +1,41 @@
+use crate::error::prelude::*;
+use std::convert::TryFrom;
+use std::fmt;
+
+#[derive(Debug, PartialEq, Deserialize)]
+pub enum CmusPlaybackStatus {
+    Playing,
+    Paused,
+    Stopped,
+}
+
+impl TryFrom<&str> for CmusPlaybackStatus {
+    type Error = Error;
+
+    fn try_from(status_str: &str) -> Result<Self, Self::Error> {
+        const STATUS_PLAYING: &str = "playing";
+        const STATUS_PAUSED: &str = "paused";
+        const STATUS_STOPPED: &str = "stopped";
+
+        match status_str.to_lowercase().as_str() {
+            STATUS_PLAYING => Ok(CmusPlaybackStatus::Playing),
+            STATUS_PAUSED => Ok(CmusPlaybackStatus::Paused),
+            STATUS_STOPPED => Ok(CmusPlaybackStatus::Stopped),
+            s => Err(Error::CmusUnknownStatus(s.into())),
+        }
+    }
+}
+
+impl fmt::Display for CmusPlaybackStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                CmusPlaybackStatus::Playing => "Playing",
+                CmusPlaybackStatus::Paused => "Paused",
+                CmusPlaybackStatus::Stopped => "Stopped",
+            }
+        )
+    }
+}

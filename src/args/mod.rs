e310b91082b21c14281d9dd4030092f64ba03a79
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-12-03 17:18:47 +0100

    Move print_* functions from main.rs to args mod

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -53,3 +53,67 @@ impl Args {
         Ok(Self { commands, options })
     }
 }
+
+pub fn print_help() {
+    let opt_help = {
+        let opt = CliOption::Help;
+        format!("-{}, --{}", opt.name_single(), opt.name_double())
+    };
+    let opt_vers = {
+        let opt = CliOption::Version;
+        format!("-{}, --{}", opt.name_single(), opt.name_double())
+    };
+    let cmd_status = CliCommand::Status.name();
+    let cmd_help = CliCommand::Help.name();
+    let cmd_dump_config = CliCommand::DumpConfig.name();
+
+    println!(
+        r#"{description}
+
+USAGE:
+    {name} [OPTIONS] [COMMAND]
+
+OPTIONS:
+    {opt_help:<opt_width$} Print this help message and exit.
+    {opt_vers:<opt_width$} Print version information and exit.
+
+COMMANDS:
+    {cmd_status}
+        Print the current cmus playback status
+        with the format configured in the config.toml file.
+        This is the default command, so you may omit this argument.
+    {cmd_dump_config}
+        Print the default config as TOML to stdout.
+        To write the default config to the proper config file, run something like:
+            mkdir -p ~/.config/{name}
+            {name} {cmd_dump_config} > ~/.config/{name}/config.toml
+    {cmd_help}
+        Print this help message and exit."#,
+        description = crate::meta::DESCRIPTION,
+        name =  crate::meta::NAME,
+        opt_width = 16,
+        opt_help = opt_help,
+        opt_vers = opt_vers,
+        cmd_status = cmd_status,
+        cmd_help = cmd_help,
+        cmd_dump_config = cmd_dump_config,
+    );
+}
+
+pub fn print_version() {
+    println!("{} v{}", crate::meta::NAME, crate::meta::VERSION)
+}
+
+pub fn dump_config() {
+    print!(
+        r#"# DEFAULT CONFIG FOR {name}
+# To write this config to the proper config file, run something like:
+#     mkdir -p ~/.config/{name}
+#     {name} {cmd_dump_config} > ~/.config/{name}/config.toml
+
+{config}"#,
+        name = crate::meta::NAME,
+        cmd_dump_config = CliCommand::DumpConfig.name(),
+        config = crate::config::DEFAULT_CONFIG
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -31,80 +31,16 @@ fn run() -> error::MyResult<()> {
     match action()? {
         Action::Status => cmus_status::print_cmus_status(),
         Action::Help => {
-            print_help();
+            args::print_help();
             Ok(())
         }
         Action::Version => {
-            print_version();
+            args::print_version();
             Ok(())
         }
         Action::DumpConfig => {
-            dump_config();
+            args::dump_config();
             Ok(())
         }
     }
-}
-
-fn print_help() {
-    let opt_help = {
-        let opt = args::CliOption::Help;
-        format!("-{}, --{}", opt.name_single(), opt.name_double())
-    };
-    let opt_vers = {
-        let opt = args::CliOption::Version;
-        format!("-{}, --{}", opt.name_single(), opt.name_double())
-    };
-    let cmd_status = args::CliCommand::Status.name();
-    let cmd_help = args::CliCommand::Help.name();
-    let cmd_dump_config = args::CliCommand::DumpConfig.name();
-
-    println!(
-        r#"{description}
-
-USAGE:
-    {name} [OPTIONS] [COMMAND]
-
-OPTIONS:
-    {opt_help:<opt_width$} Print this help message and exit.
-    {opt_vers:<opt_width$} Print version information and exit.
-
-COMMANDS:
-    {cmd_status}
-        Print the current cmus playback status
-        with the format configured in the config.toml file.
-        This is the default command, so you may omit this argument.
-    {cmd_dump_config}
-        Print the default config as TOML to stdout.
-        To write the default config to the proper config file, run something like:
-            mkdir -p ~/.config/{name}
-            {name} {cmd_dump_config} > ~/.config/{name}/config.toml
-    {cmd_help}
-        Print this help message and exit."#,
-        description = meta::DESCRIPTION,
-        name = meta::NAME,
-        opt_width = 16,
-        opt_help = opt_help,
-        opt_vers = opt_vers,
-        cmd_status = cmd_status,
-        cmd_help = cmd_help,
-        cmd_dump_config = cmd_dump_config,
-    );
-}
-
-fn print_version() {
-    println!("{} v{}", meta::NAME, meta::VERSION)
-}
-
-fn dump_config() {
-    print!(
-        r#"# DEFAULT CONFIG FOR {name}
-# To write this config to the proper config file, run something like:
-#     mkdir -p ~/.config/{name}
-#     {name} {cmd_dump_config} > ~/.config/{name}/config.toml
-
-{config}"#,
-        name = meta::NAME,
-        cmd_dump_config = args::CliCommand::DumpConfig.name(),
-        config = config::DEFAULT_CONFIG
-    );
 }
